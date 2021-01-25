e136958f459898d272b096f0f9cc9dd0c8ec635f
Mr.Radion <mr.rodion_oa@mail.ru>
2021-01-26 01:07:38 +0300

    implemented initial list of api requests

---
+++ index.js
@@ -0,0 +1,10 @@
+// import {  } from "./api";
+
+const $table = document.querySelector('.table');
+
+// let result = '';
+
+// for (let [name, value] of result) {
+//   alert(`${name} = ${value}`);
+// }
+
