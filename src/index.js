45c72a4a15cfe482bfc904676f59b31a3ec2bb47
Mr.Radion <mr.rodion_oa@mail.ru>
2021-01-26 16:18:25 +0300

    Implemented data rendering function

--- src/index.js
+++ src/index.js
@@ -3,14 +3,35 @@ import { row, col } from './utils';
 
 const $table = document.querySelector('.table');
 
-getUsers().then((res) => onUsersRecevied(res));
+getUsers()
+  .then((res) => onUsersRecevied(res))
+  .catch((error) => console.log(error));
 
+// Receiving data from the server and displaying it in a table
 function onUsersRecevied(result) {
+  let html = row({ id: col('id'), name: col('name'), email: col('email') });
+  $table.insertAdjacentHTML('beforeend', html);
+
   result.forEach((el) => {
     let id = col(el.id);
     let name = col(el.name);
     let email = col(el.email);
-    let html = row({id, name, email});
-    $table.insertAdjacentHTML('beforeend', html);
+    let rows = row({ id, name, email });
+    $table.insertAdjacentHTML('beforeend', rows);
   });
 }
+
+// Function for adding a client to a table
+
+
+
+// Table row editing function
+
+
+
+// Client data saving function
+
+
+
+// Function for removing a customer from a table
+
