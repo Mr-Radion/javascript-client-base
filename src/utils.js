953b956d4cb121f0966ccf142507c1e7e9d1e668
Mr.Radion <mr.rodion_oa@mail.ru>
2021-01-28 18:59:57 +0300

    created a library for inserting dom table elements

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,90 @@
-export function row(content) {
-  return `<tr>${[...Object.values(content)].join('')}</tr>`;
+'use strict';
+
+const objectToArray = (content) => [...Object.values(content)];
+
+// const onRenderItem = (content) => {
+//   if (typeof content === Object) {
+//     let item = [...Object.values(content)];
+//     item.forEach((elem) => `${this}`.appendChild(elem));
+//   } else if (typeof content === string || typeof content === number) {
+//  this.insertAdjacentHTML('beforeend', content);
+//   }else if (!content) {}
+// };
+
+export function table(content) {
+  let table = document.createElement('table');
+  table.className = 'table table-striped';
+
+  console.log(content);
+
+  if (content) {
+    let item = objectToArray(content);
+    item.forEach((elem) => table.appendChild(elem));
+  }
+  return table;
 }
 
-export function tableHead(content) {
-  return `<th>${content}</th>`;
+export function thead(content) {
+  let thead = document.createElement('thead');
+  if (content) {
+    let item = objectToArray(content);
+    item.forEach((elem) => thead.appendChild(elem));
+  }
+  return thead;
+}
+
+export function tbody(content) {
+  let tbody = document.createElement('tbody');
+  if (content) {
+    let item = objectToArray(content);
+    item.forEach((elem) => tbody.appendChild(elem));
+  }
+  return tbody;
+}
+
+export function row(content) {
+  let row = document.createElement('tr');
+  if (content) {
+    let item = objectToArray(content);
+    item.forEach((elem) => row.appendChild(elem));
+  }
+  return row;
+}
+
+export function thItem(content) {
+  let thItem = document.createElement('th');
+  if (content) {
+    let item = [...Object.values(content)].join('');
+    thItem.insertAdjacentHTML('beforeend', item);
+  }
+  return thItem;
 }
 
 export function col(content) {
-  return `<td>${content}</td>`;
+  let col = document.createElement('td');
+  if (content) {
+    if (typeof content === 'object') {
+      let item = content;
+      col.appendChild(item);
+    }
+    col.insertAdjacentHTML('beforeend', content);
+  }
+  return col;
 }
 
 export function link(content) {
-  return `<a>${content}</a>`;
+  let link = document.createElement('a');
+  if (content) {
+    link.insertAdjacentHTML('beforeend', content);
+  }
+  return link;
+}
+
+export function button(content, style) {
+  let button = document.createElement('button');
+  button.className = style;
+  if (content) {
+    button.insertAdjacentHTML('beforeend', content);
+  }
+  return button;
 }
